cf44d2f08bf991c2785df05c3f04b436cb47551b
Naga Srinivasa <cloudcomputingchannel@gmail.com>
2025-08-12 08:09:37 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -43,7 +43,7 @@ class HelloController {
                 </style>
             </head>
             <body>
-                <h1>Hello from <br>Welcome to Cloud Computing in Telugu</h1>
+                <h1>Hello from <br>Welcome to Cloud Computing in Telugu - CCIT</h1>
             </body>
             </html>
         """;
